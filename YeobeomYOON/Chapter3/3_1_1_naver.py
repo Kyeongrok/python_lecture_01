273a3dbd0eaeba1814eae4f202ee5b267ea8073e
ybyoonYOON <ybyoon.gg@gmail.com>
2017-11-05 18:41:35 +0900

    사이트에서 링크만 추출

--- YeobeomYOON/Chapter3/3_1_1_naver.py
+++ YeobeomYOON/Chapter3/3_1_1_naver.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen("http://pann.nate.com/")
+html = urlopen("http://news.naver.com/")
 bsObj = BeautifulSoup(html,"html.parser")
 
 wikiRefer = bsObj.findAll("a", href=re.compile("http:\/\/news.naver.com.*"))
