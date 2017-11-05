2052b0bfa1908d2e8ecfb5289acb633cfb4aba95
minduz88 <minduz88@gmail.com>
2017-11-05 17:20:49 +0900

    수업2
    span 추출 _ 김민규

--- minduz88/chapter2/2_2beautiful_soup_01.py
+++ minduz88/chapter2/2_2beautiful_soup_01.py
@@ -4,4 +4,8 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html, "html.parser")
 
-print(bsObj)
+# print(bsObj)
+
+nameList = bsObj.findAll("span")
+
+print(nameList)
