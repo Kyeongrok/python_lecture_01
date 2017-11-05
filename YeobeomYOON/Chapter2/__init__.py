9e4370707aa0df5f23e83281421e8e711f0508c1
ybyoonYOON <ybyoon.gg@gmail.com>
2017-11-05 17:06:33 +0900

    print "bye" 추가

---
+++ YeobeomYOON/Chapter2/__init__.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bs0bj = BeautifulSoup(html, "html.parser")
+print(bs0bj)
