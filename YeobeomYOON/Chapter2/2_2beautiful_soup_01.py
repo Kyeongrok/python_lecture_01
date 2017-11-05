d9dbdd461a62fe88c2bf8ee2fcef30ff0aa4c871
ybyoonYOON <ybyoon.gg@gmail.com>
2017-11-05 17:25:00 +0900

    span 가져오기

---
+++ YeobeomYOON/Chapter2/2_2beautiful_soup_01.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bs0bj = BeautifulSoup(html, "html.parser")
+
+# print(bs0bj)
+
+nameList = bs0bj.findAll("span")
+# nameList = bs0bj.findAll("span", {"class":"green"})
+# for name in nameList:
+print(nameList)
+    # print(name.get_text())
+
+# Tag란 <>로 시작해서 </>로 끝나는 것
